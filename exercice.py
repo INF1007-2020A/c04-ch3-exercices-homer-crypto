aec462215c0108a2881a5e8080c31bf67ca8c0ff
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-14 23:25:32 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def average(a: float, b: float, c: float) -> float:
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     min_to_deg = angle_mins * (1/60)
     sec_to_deg = angle_secs * (1/3600)
-    to_radians = (angle_degs + * (math.pi/180)) + min_to_rad + sec_to_rad
+    to_radians = (angle_degs + min_to_deg + sec_to_deg) * (math.pi/180)) + min_to_rad + sec_to_rad
 
     return to_radians
 
