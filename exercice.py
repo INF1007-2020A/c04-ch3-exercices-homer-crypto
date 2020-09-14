44b38e4e32477a5aa6259cbe4b8258d82cca9e7b
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-14 22:38:18 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def average(a: float, b: float, c: float) -> float:
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     min_to_rad = angle_mins*(1/60)
 
-    return Result_to_rad
+    return 0.0
 
 
 def to_degrees(angle_rads: float) -> tuple:
